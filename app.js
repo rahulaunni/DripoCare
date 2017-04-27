8dd99ec103424a9843584a6cd26101f25cf3a4c4
Personal Laptop <rahulaunni@gmail.com>
2017-04-27 13:17:50 +0530

    modified for wifi issue resolved and mqtt sending completed partially

--- app.js
+++ app.js
@@ -83,60 +83,124 @@ app.use(function(err, req, res, next) {
 module.exports = app;
 
 // mqtt part*******************************************************************************************************************
-var Account = require('./models/account');
+// var Account = require('./models/account');
+// var Station = require('./models/stations');
+// var Bed = require('./models/bed');
+// var Patient = require('./models/patient');
+// var Medication = require('./models/medication');
+// var Timetable = require('./models/timetable');
+// var mqtt = require('mqtt')
+// var client = mqtt.connect('mqtt://localhost:1883')
+// var bedg=[];
+
+// client.on('connect', function() {
+//     console.log("started");
+//     client.subscribe('dripo/#');
+//     Timetable.find({}).sort({time:1}).exec(function(err,tim){
+//     if (err) return console.error(err);
+//     var arr_bed=[];
+//     for (var key in tim) {
+//         arr_bed[key]=tim[key]._bed;
+
+//     }
+//     var arr_bed_new=[];
+//     var n=arr_bed.length;
+//     var count=0;
+//     for(var c=0;c<n;c++) //For removing duplicate bed ids 
+//         { 
+//             for(var d=0;d<count;d++) 
+//             { 
+//                 if(arr_bed[c].toString()==arr_bed_new[d].toString()) 
+//                     break; 
+//             } 
+//             if(d==count) 
+//             { 
+//                 arr_bed_new[count] = arr_bed[c]; 
+//                 count++; 
+//             } 
+//         } 
+//     Bed.find({'_id': {$in:arr_bed_new}}).populate({path:'_patient',model:'Patient',populate:{path:'_medication',model:'Medication',populate:{path:'_timetable',model:'Timetable'}}}).exec(function(err,bedd){
+//     // reordering to sorted order
+//     for (var key in arr_bed_new)
+//     {
+//         for (var key2 in bedd)
+//         {
+//             if(arr_bed_new[key].toString()==bedd[key2]._id.toString())
+//             {
+//                 bedg.push(bedd[key2])
+
+//             }
+//         }
+
+//     }
+
+// });
+// });
+
+// })
+
+
+// client.on('message', function(topic, message) {
+
+//     var res = topic.split("/");
+//     var id = res[1];
+//      console.log(JSON.stringify(bedg[0]._patient._medication[0].name));
+//     //For publishing Bed Name from database
+//     var pub_bedd=[];
+//     for (var key in bedg)
+//     {
+//       pub_bedd.push(bedg[key].bname); 
+//       pub_bedd.push('&'); 
+//       pub_bedd.push(bedg[key]._id); 
+//       pub_bedd.push('&');  
+//     }
+//     var pub_bed_slicer=pub_bedd.slice(0,19);
+//     var pub_bed=pub_bed_slicer.join('');
+//     if (res[2] == 'req') {
+//             if (message == "df") {
+//                 client.publish('dripo/' + id + '/df', "60&60&20&20&15&15&10&10&");
+//             } 
+//         }
+//     else if(res[2]=='bed_req'){
+//         if(message == "bed"){
+//             client.publish('dripo/' + id + '/bed', pub_bed);
+//         }
+//       }
+//     else if(res[2]== 'med_req'){
+//         // var medd=[];
+//         // for(var key in bedg){
+//         //     if(message==bedg[key].bname)
+//         //         for(var key2 in bedg[key]._patient._medication)
+//         //         medd.push(bedg[key]._patient._medication[key2].name)
+//         // }
+//         // console.log(JSON.stringify(medd));
+//             client.publish('dripo/' + id + '/med', "Rum&Rum&Brandy&Brandy&Beer&Beer&");
+        
+//     }
+//    else if (res[2]== 'rate_req'){
+//        if(message == "Beer"){
+//            client.publish('dripo/' + id + '/rate2set', "Patient&100&100&100&");
+//        }
+   
+    
+//  //console.log(message);
+// }
+
+// });
+
+var mqtt = require('mqtt')
+var client = mqtt.connect('mqtt://localhost:1883')
 var Station = require('./models/stations');
+var Account = require('./models/account');
 var Bed = require('./models/bed');
 var Patient = require('./models/patient');
 var Medication = require('./models/medication');
 var Timetable = require('./models/timetable');
-var mqtt = require('mqtt')
-var client = mqtt.connect('mqtt://localhost:1883')
-var bedg=[];
+var Device = require('./models/device')
 
 client.on('connect', function() {
     console.log("started");
     client.subscribe('dripo/#');
-    Timetable.find({}).sort({time:1}).exec(function(err,tim){
-    if (err) return console.error(err);
-    var arr_bed=[];
-    for (var key in tim) {
-        arr_bed[key]=tim[key]._bed;
-
-    }
-    var arr_bed_new=[];
-    var n=arr_bed.length;
-    var count=0;
-    for(var c=0;c<n;c++) //For removing duplicate bed ids 
-        { 
-            for(var d=0;d<count;d++) 
-            { 
-                if(arr_bed[c].toString()==arr_bed_new[d].toString()) 
-                    break; 
-            } 
-            if(d==count) 
-            { 
-                arr_bed_new[count] = arr_bed[c]; 
-                count++; 
-            } 
-        } 
-    Bed.find({'_id': {$in:arr_bed_new}}).populate({path:'_patient',model:'Patient',populate:{path:'_medication',model:'Medication',populate:{path:'_timetable',model:'Timetable'}}}).exec(function(err,bedd){
-    // reordering to sorted order
-    for (var key in arr_bed_new)
-    {
-        for (var key2 in bedd)
-        {
-            if(arr_bed_new[key].toString()==bedd[key2]._id.toString())
-            {
-                bedg.push(bedd[key2])
-
-            }
-        }
-
-    }
-
-});
-});
-
 })
 
 
@@ -144,74 +208,97 @@ client.on('message', function(topic, message) {
 
     var res = topic.split("/");
     var id = res[1];
-     console.log(JSON.stringify(bedg[0]._patient._medication[0].name));
-    //For publishing Bed Name from database
-    var pub_bedd=[];
-    for (var key in bedg)
-    {
-      pub_bedd.push(bedg[key].bname); 
-      pub_bedd.push('&'); 
-      pub_bedd.push(bedg[key].bname); 
-      pub_bedd.push('&');  
-    }
-    var pub_bed_slicer=pub_bedd.slice(0,19);
-    var pub_bed=pub_bed_slicer.join('');
+        Device.find({'divid':id}).exec(function(err,dev){
+        if (err) return console.error(err);
+        if (res[2] == 'req') {
+                if (message == "df") {
+                    console.log(dev[0].divid);
+                    client.publish('dripo/' + id + '/df', "60&60&20&20&15&15&10&10&");
+                } 
+            }
+        else if(res[2]=='bed_req'){
+            if(message == "bed"){
+                    Timetable.find({'station':dev[0].sname,'userid':dev[0].uid}).sort({time:1}).populate({path:'station',model:'Station'}).exec(function(err,tim){
+                    if (err) return console.error(err);
+                    var arr_bed=[];
+                    for (var key in tim) {
+                        arr_bed[key]=tim[key]._bed;
 
-if(id=="DRIPO-3574172"){
+                    }
 
-    if (res[2] == 'req') {
-            if (message == "df") {
-                client.publish('dripo/' + id + '/df', "60&60&20&20&15&15&10&10&");
-            } 
+                    var arr_bed_new=[];
+                    var n=arr_bed.length;
+                    var count=0;
+                    for(var c=0;c<n;c++) //For removing duplicate bed ids 
+                        { 
+                            for(var d=0;d<count;d++) 
+                            { 
+                                if(arr_bed[c].toString()==arr_bed_new[d].toString()) 
+                                    break; 
+                            } 
+                            if(d==count) 
+                            { 
+                                arr_bed_new[count] = arr_bed[c]; 
+                                count++; 
+                            } 
+                        } 
+                    Bed.find({'_id': {$in:arr_bed_new}}).populate({path:'_patient',model:'Patient',populate:{path:'_medication',model:'Medication',populate:{path:'_timetable',model:'Timetable'}}}).exec(function(err,bedd){
+                    // reordering to sorted order
+                    var bed=[];
+                    for (var key in arr_bed_new)
+                    {
+                        for (var key2 in bedd)
+                        {
+                            if(arr_bed_new[key].toString()==bedd[key2]._id.toString())
+                            {
+                                bed.push(bedd[key2])
+
+                            }
+                        }
+
+                    }
+                    // For publishing Bed Name from database
+                        var pub_bedd=[];
+                        for (var key in bedd)
+                        {
+                          pub_bedd.push(bedd[key].bname); 
+                          pub_bedd.push('&'); 
+                          pub_bedd.push(bedd[key]._id); 
+                          pub_bedd.push('&');  
+                        }
+                        var pub_bed_slicer=pub_bedd.slice(0,19);
+                        var pub_bed=pub_bed_slicer.join('');
+
+                        client.publish('dripo/' + id + '/bed',pub_bed);
+
+                        });
+                });
+            }
+          }
+        else if(res[2]== 'med_req'){
+            Bed.find({'_id':message}).populate({path:'_patient',model:'Patient',populate:{path:'_medication',model:'Medication'}}).exec(function(err,bedd){
+                console.log(JSON.stringify(bedd));
+            var pub_med=[];
+            for(var key in bedd._patient._medication){
+                pub_med.push(bedd[0]._patient._medication[key].name);
+                pub_bedd.push('&');
+                pub_med.push(bedd[0]._patient._medication[key]._id);
+                pub_bedd.push('&');  
+            }
+            console.log(pub_med);
+            client.publish('dripo/' + id + '/med',pub_med);
+          }); 
+
+            
         }
-    else if(res[2]=='bed_req'){
-        if(message == "bed"){
-            client.publish('dripo/' + id + '/bed', pub_bed);
-        }
-      }
-    else if(res[2]== 'med_req'){
-        // var medd=[];
-        // for(var key in bedg){
-        //     if(message==bedg[key].bname)
-        //         for(var key2 in bedg[key]._patient._medication)
-        //         medd.push(bedg[key]._patient._medication[key2].name)
-        // }
-        // console.log(JSON.stringify(medd));
-            client.publish('dripo/' + id + '/med', "Rum&Rum&Brandy&Brandy&Beer&Beer&");
+
+       // else if (res[2]== 'rate_req'){
+       //     if(message == "Beer"){
+       //         client.publish('dripo/' + id + '/rate2set', "Patient&100&100&100&");
+       //     }
+       }); 
         
-    }
-   else if (res[2]== 'rate_req'){
-       if(message == "Beer"){
-           client.publish('dripo/' + id + '/rate2set', "Patient&100&100&100&");
-       }
-   } 
     
- //console.log(message);
-}
 
 });
 
-function getDateTime() {
-
-    var date = new Date();
-
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-
-    var min = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-
-    var sec = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
-
-    var year = date.getFullYear();
-
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
-
-    var day = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
-
-    return  hour + ":" + min + ":" + sec;
-
-}
--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,9 @@ var mongoose = require('mongoose');
 var ObjectId = mongoose.Types.ObjectId;
 var wifi = require('node-wifi');
 var request=require('request');
+const wifiPassword = require('wifi-password');
+const wifiName = require('wifi-name');
+var ip = require('ip');
 function checkAuthentication(req, res, next) {
     if (req.isAuthenticated()) {
         next();
@@ -131,8 +134,19 @@ router.get('/addbed', checkAuthentication, function(req, res) {
         user: req.user
     });
 });
-
+//to get wifi password
 router.get('/adddevice', checkAuthentication, function(req, res) {
+    wifiName().then(name => {
+        var wifiname= name;
+        wifiPassword().then(password => {
+            var wifipass=password;
+            var ipaddress= ip.address();
+            console.log(' wifi SSID= ' + wifiname + ' wifi password= ' + wifipass +' ipaddress= ' + ipaddress)
+            req.session.wifissid=wifiname;
+            req.session.wifiPassword=wifipass;
+            req.session.ipaddress=ipaddress;
+        });
+    });
     wifi.init({
         iface : null // network interface, choose a random wifi interface if set to null 
     });
@@ -140,14 +154,13 @@ router.get('/adddevice', checkAuthentication, function(req, res) {
         if (err) {
             console.log(err);
         } else {
-            console.log(networks);
+            // console.log(networks);
         }
         res.render('adddevice', {
             user: req.user,
             network:networks
         });
-    });
-    
+    });  
 });
 router.get('/addwifi', checkAuthentication, function(req, res) {
     wifi.connect({ ssid : req.query.wifiname}, function(err) {
@@ -156,18 +169,26 @@ router.get('/addwifi', checkAuthentication, function(req, res) {
         }
         else{
             console.log('connected');
+            // var device_to_add = new Device({
+            //     divid: req.query.wifiname,
+            //     uid: req.user.id,
+            //     sname: req.session.station
+            // });
+            Device.collection.update({divid:req.query.wifiname},{$set:{divid:req.query.wifiname,uid:req.user.id,sname:req.session.station}},{upsert:true})
+            // device_to_add.save(function(err, device_to_add) {
+            //     if (err) return console.error(err);
+            //     else
+            //         console.log("okkk");
+            // });
         }
-    });
-    console.log(req.query.wifiname);
-        
+    });        
     });
 router.get('/connectdevice', checkAuthentication, function(req, res) {
-   // var link='http://192.168.4.1/wifisave?s=EVELABS_TECH&p=BQQJUDWB&server=192.168.0.13';
-   // console.log(link);
-   // res.(link);
-   request('http://192.168.4.1/wifisave?s=EVELABS_TECH&p=BQQJUDWB&server=192.168.0.13', function (error, response, body) {
+    console.log('http://192.168.4.1/wifisave?s='+req.session.wifissid+'&p='+req.session.wifiPassword+'&server='+req.session.ipaddress);
+   request('http://192.168.4.1/wifisave?s='+req.session.wifissid+'&p='+req.session.wifiPassword+'&server='+req.session.ipaddress, function (error, response, body) {
      console.log('error:', error); // Print the error if one occurred 
-     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+     console.log(body);
    });
 });
 router.get('/addivset', checkAuthentication, function(req, res) {
